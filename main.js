e87270a6c8ae90a505c82a43ca6471d16af40085
Dan <dan.network.17@gmail.com>
2018-06-20 23:05:12 +0300

    global fix

--- main.js
+++ main.js
@@ -1,139 +1,99 @@
-function Square(props) {
-    return (
-        <button className="square" onClick={ props.onClick }> { props.value } </button>
-);
-}
-class Board extends React.Component {
-    renderSquare(i) {
-        return (
-            <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-        />
-    );
-    }
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
 
-    render() {
-        return (
-            <div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-    </div>
-        <div className="board-row">
-            {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-    </div>
-        <div className="board-row">
-            {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-    </div>
-        </div>
-    );
-    }
-}
-class Game extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            history: [{ squares: Array(9).fill(null) }],
-            stepNumber: 0,
-            xIsNext: true
-        };
-    }
-    handleClick(i) {
-        const history = this.state.history.slice( 0, this.state.stepNumber + 1 );
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? "X" : "O";
-        this.setState({
-            history: history.concat([
-                { squares: squares }
-            ]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
-        });
-    }
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
-    jumpTo(step) {
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0 // true или false
-        });
-    }
+process.env.NODE_ENV = 'development';
 
-    render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares); // X, O или null
-        const moves = history.map((step, move) => {
-            const desc = move ? "Хід №" + move : "Почати гру";
-            return (
-                <li key={ move }>
-                <a href="#" onClick={ () => this.jumpTo(move) }> { desc } </a>
-            </li>
-        );
-        });
-        let status;
-        if (winner) {
-            status = "Переможець: " + winner;
-        } else if (this.state.stepNumber === 9 && winner === null ){
-            status = "Нічия";
-        } else {
-            status = "Зараз грає: " + ( this.state.xIsNext ? "X" : "O" );
-        }
-        return (
+let mainWindow;
+let addWindow;
 
-            <div className="game">
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
-            <div className="game-board">
-            <Board
-        squares={ current.squares }
-        onClick={ i => this.handleClick(i) }
-        />
-        </div>
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
 
-        <div className="game-info">
-            <div> { status } </div>
-            <ol> { moves } </ol>
-            </div>
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
 
-            </div>
-
-    );
-    }
+function createAddWindow(){
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Додати продукт до списку'
+    });
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    addWindow.on('close', function(){
+        addWindow.null;
+    });
 }
 
-function calculateWinner(squares) {
+ipcMain.on('item:add', function(e, item){
 
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+});
 
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
+const mainMenuTemplate = [
+    {
+        label:'Файл',
+        submenu: [
+            {
+                label: 'Додати елемент',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Видалити елемент',
+                click(){
+                    mainWindow.webContents.send('item:clear');
+                }
+            },
+            {
+                label: 'Вийти',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' :
+                    'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
     }
-    return null;
+];
 
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
 }
 
-ReactDOM.render(
-<Game />,
-    document.getElementById('root')
-);
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Інструменти розробника',
+        submenu: [
+            {
+                label: 'Переключити DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' :
+                    'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
